422ce72243ddf3b3b61e140e0e8aef89d5513a99
zhongming Fan <zhongmingfan@canway.net>
2023-03-27 17:26:14 +0800

    deploy: chart diff version

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -2,6 +2,13 @@
 
 # 部署监控对象
 object=postgres
+object_versions=("9.4.6" "9.5.2-0" "9.6" "10.0" "12.0.0" "13.0.0" "14.0.0" "15.0.0")
 
-helm install pg --namespace $object -f ./values/bitnami_values.yaml ./postgres \
---set image.tag=15.2.0
+for version in "${object_versions[@]}"; do
+    version_suffix="v${version%%.*}.${version%.*.*}"
+    version_suffix=${version_suffix//./-}
+
+    helm install pg --namespace $object -f ./values/bitnami_values.yaml ./postgres \
+    --set image.tag=$version \
+    --set commonLabels.object_version=$version_suffix
+done
