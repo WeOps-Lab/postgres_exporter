3afa9290848c8e7683bd91cadf3f02202ea947bc
zhongming Fan <zhongmingfan@canway.net>
2023-03-28 10:54:50 +0800

    deploy: pg 10.x to 10.19.0

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -2,7 +2,7 @@
 
 # 部署监控对象
 object=postgres
-object_versions=("9.4.6" "9.5.2-0" "9.6" "10.0" "11.0.0" "12.0.0" "13.0.0" "14.0.0" "15.0.0")
+object_versions=("9.4.6" "9.5.2-0" "9.6" "10.19.0" "11.0.0" "12.0.0" "13.0.0" "14.0.0" "15.0.0")
 
 for version in "${object_versions[@]}"; do
     version_suffix="v$(echo "$version" | grep -Eo '[0-9]{1,2}\.[0-9]{1,2}' | tr '.' '-')"
