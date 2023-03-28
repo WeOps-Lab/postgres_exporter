1a76d0c3766b6c12156c54d67676ef28a496b802
zhongming Fan <zhongmingfan@canway.net>
2023-03-28 14:25:59 +0800

    deploy: exporter for cluster

--- weops/pipe-tools/exporter/template/build.sh
+++ weops/pipe-tools/exporter/template/build.sh
@@ -8,6 +8,6 @@ for version in v9-4 v9-5 v9-6 v10-19 v11-0 v12-0 v13-0 v14-0 v15-0; do
   # 集群
   for architecture in primary secondary; do
     cluster_output_file="cluster_${architecture}_${version}.yaml"
-    sed "s/{{VERSION}}/${version}/g; s/{{ARCHITECTURE}}/${architecture}/g" cluster.tpl >> ../${cluster_output_file}
+    sed "s/{{VERSION}}/${version}/g; s/{{ARCHITECTURE}}/${architecture}/g" cluster.tpl >> ../cluster/${cluster_output_file}
   done
 done
