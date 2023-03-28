75f7002504b44088ab2e684afa30520f12dcc1f9
zhongming Fan <zhongmingfan@canway.net>
2023-03-28 11:15:38 +0800

    deploy: pg version 10.19.0

--- weops/pipe-tools/exporter/template/build.sh
+++ weops/pipe-tools/exporter/template/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for version in v9-4 v9-5 v9-6 v10-0 v11-0 v12-0 v13-0 v14-0 v15-0; do
+for version in v9-4 v9-5 v9-6 v10-19 v11-0 v12-0 v13-0 v14-0 v15-0; do
   # 单点
   standalone_output_file="standalone_${version}.yaml"
   sed "s/{{VERSION}}/${version}/g" standalone.tpl > ../standalone/${standalone_output_file}
