bbf080ed9ad599e1d0f8ac5524a9ec851f637eb9
zhongming Fan <zhongmingfan@canway.net>
2023-03-28 10:34:17 +0800

    deploy: pg 9.x user group

--- weops/pipe-tools/charts/helm_install.sh
+++ weops/pipe-tools/charts/helm_install.sh
@@ -7,14 +7,23 @@ object_versions=("9.4.6" "9.5.2-0" "9.6" "10.0" "11.0.0" "12.0.0" "13.0.0" "14.0
 for version in "${object_versions[@]}"; do
     version_suffix="v$(echo "$version" | grep -Eo '[0-9]{1,2}\.[0-9]{1,2}' | tr '.' '-')"
 
+    # 设置PodSecurityContext和ContainerSecurityContext
+    if [[ $version == "9.4"* ]] || [[ $version == "9.5"* ]]; then
+        pg_security_args="--set primary.podSecurityContext.fsGroup=0 --set primary.containerSecurityContext.runAsUser=0"
+    else
+        pg_security_args=""
+    fi
+
     # 单点
     helm install pg-standalone-$version_suffix --namespace $object -f ./values/bitnami_values.yaml ./postgres \
     --set image.tag=$version \
-    --set commonLabels.object_version=$version_suffix
+    --set commonLabels.object_version=$version_suffix \
+    $pg_security_args
 
     # 集群
     helm install pg-cluster-$version_suffix --namespace $object -f ./values/bitnami_values.yaml ./postgres \
     --set image.tag=$version \
     --set architecture=replication \
-    --set commonLabels.object_version=$version_suffix
+    --set commonLabels.object_version=$version_suffix \
+    $pg_security_args
 done
